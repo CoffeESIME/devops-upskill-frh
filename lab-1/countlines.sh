adfb5e8427a61cfe27ffa72514c07840f739ee7c
CoffeESIME <heromfabian@gmail.com>
2023-06-07 13:16:03 -0600

    requirements accomplish

--- lab-1/countlines.sh
+++ lab-1/countlines.sh
@@ -3,26 +3,10 @@
 while getopts "o:m:" flag; do
     case ${flag} in
     o)
-        echo "one  ${flag} ${OPTARG}"
-        echo "user  ${OPTARG}"
-        for file in *".txt"; do
-            user=$(stat -c %U "$file")
-            if [ "$user" == "$OPTARG" ]; then
-                file_array+=("$file")
-                echo "File: $file, Lines:                     $(awk 'END {print NR}' "$file") " "$file"
-            fi
-        done
+        option_o="${OPTARG}"
         ;;
     m)
-        if [ "${file_array}" ]; then
-        for file in "${file_array[@]}"; do
-            mod_month=$(date -d "$(stat -c %y "$file")" |  awk '{print $2}')
-            echo "${mod_month}"
-            if [ "$mod_month" == "$OPTARG" ]; then
-                echo "The file was last modified in June."
-            fi
-        done
-    fi
+        option_m="${OPTARG}"
         ;;
     :)
         echo "Option requires an argument." >&2
@@ -36,19 +20,55 @@ while getopts "o:m:" flag; do
             options: command info shows information about the options of the sh
             "
             read -r answer
-                if [ "$answer" = "users" ]; then
-                    ls -l | awk '{print $3}' | sort -u
-                fi
-                if [ "$answer" = "options" ]; then
-                    echo "The options for this file are 
+            if [ "$answer" = "users" ]; then
+                ls -l | awk '{print $3}' | sort -u
+            fi
+            if [ "$answer" = "options" ]; then
+                echo "The options for this file are 
                     -o) Has the argument 'name of user'
                     ex. bash countlines.sh -o fabs
                     -m) Has the argument of month it can be used to filter files by month with user or just month
                      "
-                fi
+            fi
         fi
         exit 1
         ;;
 
     esac
 done
+
+if [ -n "$option_o" ] && [ -n "$option_m" ]; then
+    for file in *".txt"; do
+        user=$(stat -c %U "$file")
+        if [ "$user" == "$option_o" ]; then
+            file_array+=("$file")
+            mod_month=$(date -d "$(stat -c %y "$file")" | awk '{print $2}')
+            echo "${mod_month}"
+            if [ "$mod_month" == "$option_m" ]; then
+                echo "File: $file, Lines:                     $(awk 'END {print NR}' "$file") " "$file"
+            fi
+        fi
+    done
+    exit 1
+fi
+
+if [ -n "$option_o" ]; then
+    echo "Option -a was provided with argument: $option_o"
+    for file in *".txt"; do
+        user=$(stat -c %U "$file")
+        if [ "$user" == "$OPTARG" ]; then
+            echo "File: $file, Lines:                     $(awk 'END {print NR}' "$file") " "$file"
+        fi
+    done
+fi
+
+if [ -n "$option_m" ]; then
+    echo "Option -b was provided with argument: $option_m"
+    for file in *".txt"; do
+        mod_month=$(date -d "$(stat -c %y "$file")" | awk '{print $2}')
+        echo "${mod_month}"
+        if [ "$mod_month" == "$option_m" ]; then
+            echo "File: $file, Lines:                     $(awk 'END {print NR}' "$file") " "$file"
+        fi
+    done
+fi
